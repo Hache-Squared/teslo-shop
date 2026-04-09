406f43dd9c80f5ba5dadb682bb0bb42eb82d723e
Angel Gabriel <aghh.oficial@gmail.com>
2026-04-09 15:44:03 -0600

    Added FileFilter

---
+++ src/files/files.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( 
+    FileInterceptor(
+      'file',
+      {
+        fileFilter: fileFilter,
+        // limits: { fieldSize: 1000 }
+        storage: diskStorage({
+          destination: './static/products'
+        })
+      }
+    ) 
+  ) 
+  uploadProductFile( 
+    @UploadedFile() file: Express.Multer.File
+  ){
+    
+    if(!file){
+      throw new BadRequestException('Make sure the file is an image');
+    }
+    
+    return file.originalname;
+  }
+}
