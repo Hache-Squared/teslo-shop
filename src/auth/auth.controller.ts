c84473c2863606b8828214fccaadc7d70eab2a07
Angel Gabriel <aghh.oficial@gmail.com>
2026-04-11 15:59:28 -0600

    added module jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto'
 
 
 @Controller('auth')
@@ -10,6 +10,11 @@ export class AuthController {
   @Post('register')
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.authService.create(createUserDto);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto){
+    return this.authService.login(loginUserDto);
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt'
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -32,6 +33,27 @@ export class AuthService {
       this.handleExceptions(error)
       
     }
+  }
+
+  async login(loginUserDto: LoginUserDto){
+    
+    const { email, password } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where:  { email },
+      select: {
+        email: true,
+        password:  true
+      }
+    })
+
+    if(!user){
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+    if(!bcrypt.compareSync(password, user.password)){
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+    return user;
+    //Retornar token
   }
 
 
