11bf44c6326375525c6e4c1207259422924f07cc
Angel Gabriel <aghh.oficial@gmail.com>
2026-04-13 19:50:44 -0600

    Added documentation example

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min, MinLength } from "class-validator";
 
 export class PaginationDto {
+    @ApiProperty({
+        default: 10,
+        description: 'How many rows you need'
+    })
     @IsOptional()
     @IsPositive()
     @Type(() => Number)
     limit?: number;
 
+    @ApiProperty({
+        default: 0,
+        description: 'How many rows you want to skip'
+    })
     @IsOptional()
     @Min(0)
     @Type(() => Number)
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,49 +1,86 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 import { User } from "src/auth/entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({
     name: 'products'
 })
 export class Product {
 
+    @ApiProperty({
+        example: '237b2d4d-a44a-47df-a350-c1407b86b015',
+        description: 'Product ID',
+        uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string;
-    
+
+    @ApiProperty({
+        example: 'T-Shit Teslo',
+        description: 'Product Title',
+        uniqueItems: true
+    })    
     @Column('text',{
         unique: true
     })
     title: string;
 
+    @ApiProperty({
+        example: 0,
+        description: 'Product Price',
+    })
     @Column('float',{
         default: 0
     })
     price: number;
 
+    @ApiProperty({
+        example: 'Lorem',
+        description: 'Product description'
+    })
     @Column({
         type: 'text',
         nullable: true
     })
     description: string;
 
+    @ApiProperty({
+        example: 't_shirt',
+        description: 'Product SLUG - For SEO',
+    })
     @Column('text',{
         unique: true
     })
     slug: string;
 
+    @ApiProperty({
+        example: 10,
+        description: 'Product stock',
+        default : 0
+    })
     @Column('int',{
         default: 0
     })
     stock: number;
-
+    
+    @ApiProperty({
+        example: ['M','XL','XXL'],
+        description: 'Product SLUG - For SEO',
+    })
     @Column('text',{
         array: true
     })
     sizes: string[];
 
+    @ApiProperty({
+        example: 'women',
+        description: 'Product SLUG - For SEO'
+    })
     @Column('text')
     gender: string
 
+    @ApiProperty()
     //tags
     @Column('text', {
         array: true,
